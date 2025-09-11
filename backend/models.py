1a0970774e430dddbd81be11311696ebaa910772
RajAmin99 <rajamin2004@gmail.com>
2025-09-11 16:30:08 +0100

    Database Testing

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
+
 
 class Word(Base):
     __tablename__ = "words"
@@ -9,3 +11,14 @@ class Word(Base):
     language = Column(String)
     origin = Column(String)
     history = Column(String)
+
+"""
+class Etymology(Base):
+    __tablename__ = "etymologies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    word_id = Column(Integer, ForeignKey("words.id"), nullable=False)
+    etymology = Column(String, nullable=False)
+
+    word = relationship("Word", back_populates="etymologies")
+"""
